128f4dbed6f800a71184868ff23d60a78e4ee800
Muthuprasanth Murugesan <muthuprasanthm@INLM-MUTHU.local>
2018-11-30 12:57:02 +0530

    2nd commit

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/tasks', function(req, res, next) {
+  console.log("ResumeScreening AI")
   res.render('index', { title: 'Express' });
 });
 
